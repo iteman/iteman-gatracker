6484b02bb85c07dfd3a004c7149c7794e8b17d54
KUBO Atsuhiro <kubo@iteman.jp>
2009-04-05 02:33:03 +0900

    - ext_dir, php_bin パラメータの設定を行わないようにした。 - www_dir パラメータの値を imports/PEAR/www に変更した。 - 不要になったため LPEAR_PHP_INSTALL_DIR 変数を削除した。 - ライセンス文を追加した。

--- bin/lpear.bat
+++ bin/lpear.bat
@@ -1,8 +1,29 @@
 @ECHO OFF
-REM $Id$
+REM Copyright (c) 2009 ITEMAN, Inc. All rights reserved.
+REM
+REM Redistribution and use in source and binary forms, with or without
+REM modification, are permitted provided that the following conditions are met:
+REM
+REM     * Redistributions of source code must retain the above copyright
+REM       notice, this list of conditions and the following disclaimer.
+REM     * Redistributions in binary form must reproduce the above copyright
+REM       notice, this list of conditions and the following disclaimer in the
+REM       documentation and/or other materials provided with the distribution.
+REM
+REM THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+REM AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+REM IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+REM ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+REM LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+REM CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+REM SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+REM INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+REM CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+REM ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+REM POSSIBILITY OF SUCH DAMAGE.
 
 REM *************************************************************
-REM ** The project local PEAR for Windows based systems (based on symfony.bat)
+REM ** A pear command wrapper for project local PEAR environments for Windows based systems (based on symfony.bat)
 REM *************************************************************
 
 REM This script will do the following:
@@ -20,21 +41,18 @@ popd
 
 SET PHP_PEAR_INSTALL_DIR=%TARGET_PATH%\imports\PEAR
 SET PHP_PEAR_BIN_DIR=%TARGET_PATH%\bin
-SET LPEAR_PHP_INSTALL_DIR=C:\win32app\php-5.2.6-Win32
 
 IF NOT EXIST %SCRIPT_DIR%pear.ini (
    @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set bin_dir %TARGET_PATH%\bin user
    @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set doc_dir %PHP_PEAR_INSTALL_DIR%\docs user
-   @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set ext_dir %LPEAR_PHP_INSTALL_DIR%\ext user
    @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set php_dir %PHP_PEAR_INSTALL_DIR% user
    @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set cache_dir %TARGET_PATH%\tmp\pear\cache user
    @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set cfg_dir %PHP_PEAR_INSTALL_DIR%\cfg user
    @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set data_dir %PHP_PEAR_INSTALL_DIR%\data user
    @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set download_dir %TARGET_PATH%\tmp\pear\download user
-   @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set php_bin %LPEAR_PHP_INSTALL_DIR%\php.exe user
    @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set temp_dir %TARGET_PATH%\tmp\pear\temp user
    @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set test_dir %PHP_PEAR_INSTALL_DIR%\tests user
-   @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set www_dir %TARGET_PATH%\www user
+   @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini config-set www_dir %PHP_PEAR_INSTALL_DIR%\www user
 )
 
 @call %PHP_PEAR_BIN_DIR%\pear.bat -c %SCRIPT_DIR%pear.ini %1 %2 %3 %4 %5 %6 %7 %8 %9
