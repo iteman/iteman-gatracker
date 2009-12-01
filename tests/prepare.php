60bb4b7a593718526e37b7f1a5dc2bf783a740e0
KUBO Atsuhiro <kubo@iteman.jp>
2009-12-02 04:03:30 +0900

    マイナーな改善を行った。

--- tests/prepare.php
+++ tests/prepare.php
@@ -35,6 +35,9 @@
  */
 
 error_reporting(E_ALL | E_STRICT);
+if (defined('E_DEPRECATED')) {
+    error_reporting(error_reporting() & ~E_DEPRECATED);
+}
 
 set_include_path(realpath(dirname(__FILE__)) . PATH_SEPARATOR .
                  realpath(dirname(__FILE__) . '/../src') . PATH_SEPARATOR .
